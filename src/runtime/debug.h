bd360b88f59b6fa9f4d5d265ac5df551c31afcf5
Serhii Khoma <sergey@nordicresults.com>
2026-06-18 08:10:49 +0700

    feat: remove dead <iostream> and <typeinfo> from runtime/debug.h

--- src/runtime/debug.h
+++ src/runtime/debug.h
@@ -5,8 +5,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Author: Leonardo de Moura
 */
 #pragma once
-#include <iostream>
-#include <typeinfo>
 #include "runtime/exception.h"
 
 #ifndef __has_builtin
