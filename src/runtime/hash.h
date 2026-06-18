db6f326fac516c2e20ef3fbb8309f3985323a5c2
Serhii Khoma <sergey@nordicresults.com>
2026-06-18 08:09:35 +0700

    feat: remove dead runtime/debug.h from runtime/hash.h

--- src/runtime/hash.h
+++ src/runtime/hash.h
@@ -5,7 +5,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Author: Leonardo de Moura
 */
 #pragma once
-#include "runtime/debug.h"
 #include "runtime/int.h"
 
 namespace lean {
