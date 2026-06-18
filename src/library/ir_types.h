28e54c07e5306349f60d18e80c48d85948ea7537
Serhii Khoma <sergey@nordicresults.com>
2026-06-18 08:04:52 +0700

    feat: replace overly broad elab_environment.h with minimal includes in ir_types.h

--- src/library/ir_types.h
+++ src/library/ir_types.h
@@ -5,7 +5,8 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Author: Leonardo de Moura
 */
 #pragma once
-#include "library/elab_environment.h"
+#include "runtime/object_ref.h"
+#include "util/name.h"
 namespace lean {
 namespace ir {
 /*
