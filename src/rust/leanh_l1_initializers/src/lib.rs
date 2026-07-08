64bae86ca47eec159399445a99cdef2f178520d7
Serhii Khoma <sergey@nordicresults.com>
2026-07-08 23:36:24 +0700

    init

---
+++ src/rust/leanh_l1_initializers/src/lib.rs
@@ -0,0 +1,7 @@
+#![allow(non_upper_case_globals, unsafe_op_in_unsafe_fn)]
+
+#[cfg(not(target_pointer_width = "64"))]
+compile_error!("Lean Rust support crates require 64-bit pointer width");
+
+pub mod emitted;
+pub mod r#priv;
---
+++ src/rust/leanh_l1_initializers/src/priv.rs
@@ -0,0 +1,1 @@
+// pub mod dec_for_del;
